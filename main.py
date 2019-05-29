d19e1e723340631bd07b07cfac1bdac344a92f11
weisuhua <51103527+weisuhua@users.noreply.github.com>
2019-05-29 09:10:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import time
 import MQTT
 import machine
 
-print('A')
-print('B')
 print('*******adding from PythonA********')
 print('***********this is a project**********')
 print('**************adding now*****************')
 print('*******adding from master branch********')
 print('*******adding from PythonD branch********')
+print('***********my name is weisuhua*****************')
